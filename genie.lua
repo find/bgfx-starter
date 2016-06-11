28a1efd92335d3f9f63cff02c4bd82054d772936
if yu <if yu>
2016-06-11 22:21:47 +0800

    bare bone

---
+++ genie.lua
@@ -0,0 +1,49 @@
+ROOT_DIR = path.getabsolute('.')
+BGFX_DIR = path.join(ROOT_DIR, '3rdparty/bgfx')
+BX_DIR = path.join(ROOT_DIR, '3rdparty/bx')
+SPDLOG_DIR = path.join(ROOT_DIR, '3rdparty/spdlog')
+local BUILD_DIR = ".build"
+local LIB_DIR = ".build/lib"
+
+
+solution('bgfx-starter')
+    configurations({'Debug','Release'})
+    platforms({'x32','x64','Native'})
+    language('C++')
+    startproject('starter')
+
+    dofile (path.join(BX_DIR, "scripts/toolchain.lua"))
+    if not toolchain(BUILD_DIR, LIB_DIR) then
+        return -- no action specified
+    end
+
+
+-- project bgfx
+function copyLib() end
+dofile (path.join(BGFX_DIR, 'scripts/bgfx.lua'))
+bgfxProject("", "StaticLib", {})
+
+-- project example-common
+dofile (path.join(BGFX_DIR, 'scripts/example-common.lua'))
+
+-- project starter
+project('starter')
+    kind('WindowedApp')
+    files({'src/**.h', 'src/**.cpp'})
+    includedirs({
+        path.join(BGFX_DIR, 'include'),
+        path.join(BGFX_DIR, 'examples/common'),
+        path.join(BGFX_DIR, '3rdparty'),
+        path.join(BX_DIR, 'include'),
+        path.join(SPDLOG_DIR, 'include')
+    })
+    links({
+        'bgfx',
+        'example-common'
+    })
+	configuration({ "vs20*", "x32 or x64" })
+		links {
+			"gdi32",
+			"psapi",
+		}
+
---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include "bx/uint32_t.h"
+#include "entry/entry.h"
+#include "entry/input.h"
+
+#include "bgfx/bgfx.h"
+#include "bgfx_utils.h"
+#include <imgui/imgui.h>
+#include <imgui/ocornut_imgui.h>
+
+class ExampleHelloWorld : public entry::AppI
+{
+	void init(int _argc, char** _argv) BX_OVERRIDE
+	{
+		Args args(_argc, _argv);
+
+		m_width = 1280;
+		m_height = 720;
+		m_debug = 0;
+		m_reset = BGFX_RESET_VSYNC;
+
+		bgfx::init(args.m_type, args.m_pciId);
+		bgfx::reset(m_width, m_height, m_reset);
+
+		// Enable debug text.
+		bgfx::setDebug(m_debug);
+
+		// Set view 0 clear state.
+		bgfx::setViewClear(0
+			, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH
+			, 0x303030ff
+			, 1.0f
+			, 0
+		);
+
+		imguiCreate();
+	}
+
+	virtual int shutdown() BX_OVERRIDE
+	{
+		imguiDestroy();
+
+		// Shutdown bgfx.
+		bgfx::shutdown();
+
+		return 0;
+	}
+
+	bool update() BX_OVERRIDE
+	{
+		if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState)) {
+			uint8_t const* input = inputGetChar();
+			imguiBeginFrame(m_mouseState.m_mx, m_mouseState.m_my,
+				  (m_mouseState.m_buttons[entry::MouseButton::Left] ? IMGUI_MBUT_LEFT : 0)
+				| (m_mouseState.m_buttons[entry::MouseButton::Right] ? IMGUI_MBUT_RIGHT : 0)
+				| (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0), 
+				m_mouseState.m_mz, m_width, m_height, input?*input:0);
+
+			// Set view 0 default viewport.
+			bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
+
+			// This dummy draw call is here to make sure that view 0 is cleared
+			// if no other draw calls are submitted to view 0.
+			bgfx::touch(0);
+
+			// Use debug font to print information about this example.
+			bgfx::dbgTextClear();
+			bgfx::dbgTextPrintf(0, 1, 0x4f, "helloworld");
+			bgfx::dbgTextPrintf(0, 2, 0x6f, "Initialization and setup imgui.");
+
+			ImGui::SetNextWindowSizeConstraints(ImVec2(200, 100), ImVec2(m_width, m_height));
+			ImGui::Begin("ImGui");
+			ImGui::Text("Hello!");
+			ImGui::DragFloat("sample var", &m_sampleVar, 0.1f, 0.0f, 100.0f);
+			ImGui::End();
+
+			imguiBeginScrollArea("whatever", 400, 100, 200, 400, &m_whateverscoll);
+			imguiLabel("Hello!");
+			imguiSlider("sample var", m_sampleVar, 0.0f, 100.0f, 0.1f);
+			imguiEndScrollArea();
+
+			// IMGUI_endFrame();
+			imguiEndFrame();
+
+			// Advance to next frame. Rendering thread will be kicked to
+			// process submitted rendering primitives.
+			bgfx::frame();
+
+			return true;
+		}
+
+		return false;
+	}
+	
+	entry::MouseState m_mouseState;
+	float m_sampleVar = 0.0f;
+
+	uint32_t m_width;
+	uint32_t m_height;
+	uint32_t m_debug;
+	uint32_t m_reset;
+	int m_whateverscoll = 0;
+};
+
+ENTRY_IMPLEMENT_MAIN(ExampleHelloWorld);
